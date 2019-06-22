e0afdfa15ea4e5775d1be7cbaba7b305899dacd1
akashdeepb <akashdeepb.cs17@sahyadri.edu.in>
2019-06-22 17:23:09 -0400

    Implemented Clicker

--- src/AutoClickBot.java
+++ src/AutoClickBot.java
@@ -1,17 +1,25 @@
 import java.awt.*;
+import java.awt.event.InputEvent;
 import java.util.ArrayList;
 
 class AutoClickBot {
     AutoClickBot(ArrayList x, ArrayList y) throws Exception{
         try {
+            int index = 0;
             Robot clicker = new Robot();
-            clicker.mouseMove(Integer.parseInt(x.get(0).toString()),Integer.parseInt(y.get(0).toString()));
-            try{
-                Thread.sleep(2000);
-            }catch (InterruptedException ex){
-                throw new Exception("Interrupted By User");
+            for(int i = 0; i < 50; i++) {
+                while (index < x.size()) {
+                    clicker.mouseMove(Integer.parseInt(x.get(index).toString()), Integer.parseInt(y.get(index).toString()));
+                    clicker.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                    clicker.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ex) {
+                        throw new Exception("Interrupted By User");
+                    }
+                    i++;
+                }
             }
-            clicker.mouseMove(Integer.parseInt(x.get(1).toString()), Integer.parseInt(y.get(1).toString()));
 
         }catch(AWTException ex){
             throw new Exception("Failed to Create Bot");
